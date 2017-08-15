9c9c78e22bbf24001908df21adba0d5e40bc4097
octavio <egeye4y@163.com>
2017-08-15 18:13:52 +0800

    clear

--- collect/src/com/ffcs/bdemo/HelloController.java
+++
@@ -1,9 +0,0 @@
-package com.ffcs.bdemo;
-
-import com.jfinal.core.Controller;
-
-public class HelloController extends Controller {
-    public void hello() {
-        renderText("what?????");
-    }
-}
